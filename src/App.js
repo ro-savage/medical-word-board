728e25d8d83226722469fb2a4e3beb95d78557cf
Ro Savage <rowan.savage@gmail.com>
2017-03-03 01:06:29 +1100

    Added empty space

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      selectedWord: '',
+      selectedWord: ' ',
       wordList: defaultList,
       bodyPart: false,
     }
