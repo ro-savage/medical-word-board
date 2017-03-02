645c94ca06ce74ab541b696c4f12ef79c37ef94e
Ro Savage <rowan.savage@gmail.com>
2017-03-03 02:35:04 +1300

    Added body parts

--- src/App.js
+++ src/App.js
@@ -2,19 +2,41 @@ import React, { Component } from 'react';
 
 import styles from "./App.css"
 
+const defaultList = ['Yes', 'No', 'Help', 'Pain', 'I understand', 'I don\'t understand', 'Say again', 'Uncomfortable', 'Clean me']
+const bodyPart = ['Head', 'Left Arm', 'Left Leg', 'Right Arm', 'Right Leg', 'Chest', 'Stomach', 'Genitals', 'Everywhere',]
+
+
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      selectedWord: 'Test word',
-      wordList: ['Yes', 'No', 'Help', 'Pain', 'I understand', 'Do not understand', 'Say again', 'Uncomfortable', 'Clean me']
+      selectedWord: '',
+      wordList: defaultList,
+      bodyPart: false,
     }
   }
 
   onClick = (word) => {
-    this.setState({
-      selectedWord: word,
-    })
+
+    if (word === 'Pain' || word === 'Uncomfortable') {
+      this.setState({
+        selectedWord: word,
+        wordList: bodyPart,
+        bodyPart: true,
+      })
+    } else if (this.state.bodyPart) {
+      this.setState({
+        selectedWord: this.state.selectedWord + ' - ' + word,
+        wordList: defaultList,
+        bodyPart: false,
+      })
+    } else {
+      this.setState({
+        selectedWord: word,
+        wordList: defaultList,
+        bodyPart: false,
+      })
+    }
   }
 
   render() {
